6a668feadc3a0cb2095bee05914e1675abf508b7
Austin Noto-Moniz <metalnut4@netscape.net>
2022-01-11 06:19:58 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='fourthand1core',
+    version="0.1",
+    author="Austin Noto-Moniz",
+    author_email="mathfreak65@gmail.com",
+    packages=find_packages(),
+    python_requires=">=3.6"
+)
+
